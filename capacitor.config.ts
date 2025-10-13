65c821ed09f8dd186c928969ed238858fc59110a
Dmitry Sterlin <uglenpish@rambler.ru>
2025-10-13 16:17:44 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fintask-frontend',
+  webDir: 'dist'
+};
+
+export default config;
